7e74e05afe27919a9b5d1bdb6671768c41f95e8c
Rob Bednark <rbednark@gmail.com>
2025-12-17 13:58:40 -0800

    backup: 2025-12-17 -- I changed this setting: `REAPER Preferences > Appearance > Zoom/Scroll/Offset > Horizontal zoom center > Edit cursor or play cursor (default)`

--- backup.sh
+++ backup.sh
@@ -4,6 +4,7 @@ set -euvx
 date
 cp -pr symlink-Library-Application_Support-REAPER/* .
 
-echo 'Note that "cp -pr" copies symlinks as files/dirs, NOT as symlinks, so it's safe to "rm" here.'
+echo 'Note that "cp -pr" copies symlinks as files/dirs, NOT as symlinks, so it is safe to "rm" here.'
 rm -fr ColorThemes
 rm -fr Data
+git status
