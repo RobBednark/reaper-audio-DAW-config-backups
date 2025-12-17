3a2c18d49fa0334a4369b6ebe27d413f46ec8cc8
Rob Bednark <rbednark@gmail.com>
2025-12-17 13:52:18 -0800

    backup Wed 2025-12-17 1:25pm

--- backup.sh
+++ backup.sh
@@ -3,5 +3,7 @@
 set -euvx
 date
 cp -pr symlink-Library-Application_Support-REAPER/* .
+
+echo 'Note that "cp -pr" copies symlinks as files/dirs, NOT as symlinks, so it's safe to "rm" here.'
 rm -fr ColorThemes
 rm -fr Data
