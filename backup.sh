83a2fa3131c328d2fee75c918458277010d8a58e
Rob Bednark <rbednark@gmail.com>
2025-12-19 18:43:18 -0800

    backup.sh: update

--- backup.sh
+++ backup.sh
@@ -2,7 +2,15 @@
 
 set -euvx
 date
-cp -pr symlink-Library-Application_Support-REAPER/* .
+
+DIR_FROM="symlink-Library-Application_Support-REAPER"
+DIR_TO=./backup_copied_from_home_Library
+
+git status
+mkdir -p $DIR_TO
+ls -l $DIR_FROM
+cd $DIR_TO
+cp -pr DIR_FROM/* ./
 
 echo 'Note that "cp -pr" copies symlinks as files/dirs, NOT as symlinks, so it is safe to "rm" here.'
 rm -fr ColorThemes
